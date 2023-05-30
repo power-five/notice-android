9a54ddeb0dcc07482094845ad0d267dff256b1bc
Sujin Kim <sujinkim919@gmail.com>
2023-05-30 22:29:41 +0900

    feat: 우측 하단 글쓰기 아이콘 추가

--- app/src/main/java/com/woowa/notice/ui/noticelist/NoticeListActivity.kt
+++ app/src/main/java/com/woowa/notice/ui/noticelist/NoticeListActivity.kt
@@ -1,6 +1,7 @@
 package com.woowa.notice.ui.noticelist
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -21,12 +22,24 @@ class NoticeListActivity : AppCompatActivity(), NoticeListContract.View {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        presenter.getNoticeList()
-        setRecyclerViewDivider()
+        initNoticeList()
+        setAddNoticeButtonListener()
     }
 
     override fun initRecyclerView(noticeItems: List<NoticeUiModel>) {
         binding.rvNoticeList.adapter = NoticeListAdapter(noticeItems)
+    }
+
+    private fun initNoticeList() {
+        presenter.getNoticeList()
+        setRecyclerViewDivider()
+    }
+
+    private fun setAddNoticeButtonListener() {
+        binding.btnAddNotice.setOnClickListener {
+            // TODO: 공지사항 추가 화면으로 이동
+            Toast.makeText(this, "공지사항 추가 화면으로 이동", Toast.LENGTH_SHORT).show()
+        }
     }
 
     private fun setRecyclerViewDivider() {
