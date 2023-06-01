9735fe8de17b1c68f4246ac49e2e69a47bd137e8
Yerin <myheaven_b@naver.com>
2023-06-01 16:01:51 +0900

    feat: NoticeWritingActivity view 구현

---
+++ app/src/main/java/com/woowa/notice/ui/noticewriting/NoticeWritingActivity.kt
@@ -0,0 +1,12 @@
+package com.woowa.notice.ui.noticewriting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.woowa.notice.R
+
+class NoticeWritingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_notice_writing)
+    }
+}
